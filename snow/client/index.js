580d460590561e71949aaf1988dabaf81144467a
ChriiiZn <106380960+ChriiiZn@users.noreply.github.com>
2022-11-28 19:16:24 +0100

    Update index.js

--- snow/client/index.js
+++ snow/client/index.js
@@ -2,12 +2,12 @@ import { on } from 'alt-client'
 import { setForcePedFootstepsTracks, setForceVehicleTrails, useParticleFxAsset, requestScriptAudioBank, requestNamedPtfxAsset } from "natives";
 
 on("connectionComplete", () => {
-  //  -- Weather physic start ---
+  //  --- Weather physic start ---
   setForcePedFootstepsTracks(true);
   useParticleFxAsset("core_snow");
   setForceVehicleTrails(true)
   requestScriptAudioBank("ICE_FOOTSTEPS", false, 0)
   requestScriptAudioBank("SNOW_FOOTSTEPS", false, 0)
   requestNamedPtfxAsset("core_snow");
-   // -- Weather physic end ---
+   // --- Weather physic end ---
 });
